8c68cb679ecb7c63ceddc14b604d5fe4059617ca
GodDamndelion <bolshakov.pavel.alekseevich@yandex.ru>
2023-11-29 00:56:10 +0300

    Добавлены AutoMapper, Cors, Swagger и Versioning

---
+++ Systems/EventGenerator/EGAH.EventGenerator/Configuration/ControllerAndViewsConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace EGAH.EventGenerator.Configuration;
+
+using EGAH.Common;
+
+public static class ControllerAndViewsConfiguration
+{
+    public static IServiceCollection AddAppControllers(this IServiceCollection services)
+    {
+        services
+            .AddRazorPages();
+
+        services
+            .AddControllers()
+            .AddNewtonsoftJson(options => options.SerializerSettings.SetDefaultSettings())
+            //.AddValidator() Пока отсутствует
+            // TODO: Добавить сюда Validator
+            ;
+
+        return services;
+    }
+
+    public static IEndpointRouteBuilder UseAppControllers(this IEndpointRouteBuilder app)
+    {
+        app.MapRazorPages();
+        app.MapControllers();
+
+        return app;
+    }
+}
--- Systems/EventGenerator/EGAH.EventGenerator/Program.cs
+++ Systems/EventGenerator/EGAH.EventGenerator/Program.cs
@@ -2,23 +2,25 @@ using EGAH.EventGenerator.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.AddAppLogger();
-
-// Configure services
 
 var services = builder.Services;
 
+services.AddHttpContextAccessor();
+services.AddAppCors();
+
 services.AddAppHealthChecks();
+services.AddAppVersioning();
+services.AddAppSwagger();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-app.UseAppHealthChecks();
+app.UseAppCors();
 
-// Configure the HTTP request pipeline.
+app.UseAppHealthChecks();
+app.UseAppSwagger();
 
 app.UseAuthorization();
 
